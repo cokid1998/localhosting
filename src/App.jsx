f20a8a0baac7632311ce8bee6fd39b6cba15bc9d
cokid1998 <cokid98@naver.com>
2024-10-06 23:51:04 +0900

    Chore: 라우팅 설명 주석

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
+import { Route, Routes } from "react-router-dom";
 import Layout from "@/components/Layout/Layout";
 import Test from "@/page/Test";
 import Main from "@/page/Main";
-import { Route, Routes } from "react-router-dom";
+import Hello from "@/page/Hello";
 
 function App() {
   return (
@@ -13,5 +14,14 @@ function App() {
     </Layout>
   );
 }
+
+// 라우팅을 위한 코드들입니다.
+// Route라는 컴포넌트의 props에 path는 url을 지정, element는 path로 지정한 주소에 맞는 컴포넌트를 보여줍니다.
+// (꺽쇠안에 path, element같은걸 props라고 부름)
+// 예를 들어 <Route path="/hello" element={<Hello />} />를 추가하면
+// @/page/Hello경로의 파일을 화면에 보여줍니다.
+// <Route path="/hello" element={<Hello />} />를 위 코드에 추가하고 아래 주소로 방문해보세요!
+// http://localhost:5173/hello
+// 🚨<주의>🚨 Route는 Routes안에 넣어줘야합니다.
 
 export default App;
