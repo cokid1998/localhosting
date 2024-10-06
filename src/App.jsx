bed1cc4d72c416319c337161faab00b066aecac8
cokid1998 <cokid98@naver.com>
2024-10-06 19:10:46 +0900

    Feat: default router setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import "./App.css";
 import Test from "@/page/Test";
+import Main from "@/page/Main";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Test />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/test" element={<Test />} />
+      </Routes>
     </>
   );
 }
