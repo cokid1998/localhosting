8150e874a06fcae0e977921124d1db5514d733f7
cokid1998 <cokid98@naver.com>
2024-10-17 20:37:25 +0900

    chore: Add shadcn

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,21 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 const Navbar = () => {
-  return <div className="absolute bottom-[10px]">navbar</div>;
+  return (
+    <div className="absolute bottom-[10px]">
+      <Tabs defaultValue="account" className="w-[400px]">
+        <TabsList>
+          <TabsTrigger value="account">Account</TabsTrigger>
+          <TabsTrigger value="password">Password</TabsTrigger>
+          <TabsTrigger value="password">Password</TabsTrigger>
+        </TabsList>
+        <TabsContent value="account">
+          Make changes to your account here.
+        </TabsContent>
+        <TabsContent value="password">Change your password here.</TabsContent>
+      </Tabs>
+    </div>
+  );
 };
 
 export default Navbar;
