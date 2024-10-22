5dec1fd2dc8042bcb0fbc78d0c61d756e7854e02
okeyeyo <clskfk1234@naver.com>
2024-10-22 22:34:03 +0900

    feat : add join nav

--- src/page/Join.jsx
+++ src/page/Join.jsx
@@ -1,2 +1,28 @@
 import React from "react";
-import styles from "@/components/Layout/CafeList.module.css";
+import styles from "@/components/Layout/Join.module.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { ChevronLeft } from "lucide-react";
+
+function Join() {
+  const Router = () => {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </BrowserRouter>
+    );
+  };
+  return (
+    <div className={styles.container}>
+      <div className={styles.navbar}>
+        <Link to="/">
+          <ChevronLeft size={24} color="black" />
+        </Link>
+        <div className={styles.title}>회원가입</div>
+      </div>
+    </div>
+  );
+}
+
+export default Join;
