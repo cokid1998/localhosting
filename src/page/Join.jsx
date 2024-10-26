559ece15a09d29062e2b951453bbc083630b34dd
cokid1998 <cokid98@naver.com>
2024-10-26 14:44:04 +0900

    Add comment

--- src/page/Join.jsx
+++ src/page/Join.jsx
@@ -4,6 +4,16 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { ChevronLeft } from "lucide-react";
 import { useState } from "react";
 
+function ThreeDot() {
+  return (
+    <>
+      <div className={styles.dot}></div>
+      <div className={styles.dot}></div>
+      <div className={styles.dot}></div>
+    </>
+  );
+}
+
 function Join() {
   const Router = () => {
     return (
@@ -58,6 +68,7 @@ function Join() {
         <div className={styles.dot}></div>
         <div className={styles.dot}></div>
         <div className={styles.dot}></div>
+        {/* <ThreeDot/> */}
         <div
           className={`${styles.step} ${currentStep >= 3 ? styles.active : ""}`}
           onClick={() => setCurrentStep(3)}
@@ -74,3 +85,16 @@ function Join() {
 }
 
 export default Join;
+
+// 8번째 줄 Router함수 지워주세요
+
+// Join으로된 파일들 이름 Signup으로 바꿔주세요(Join.jsx, Join.module.css, App.jsx파일에서도 /join이 아니라 /signup으로 바꾸세요) 회원가입관련 파일들의 이름은 Signup이 많이쓰입니다.
+
+// 다음버튼은 div태그가아니라 button태그를 사용하세요. (동작은 똑같지만 button이라는 명확한 태그를 사용하는게 좋습니다.)
+
+// 태그안에 내용이없다면 바로 닫아도 됩니다. <div></div> == <div/> Dog태그 모두 이렇게 바꾸세요
+
+// Step이 1이 아닐 때는 이전 버튼도 나오게 만들어보세요 아직 디자인이 안된거 같지만 있어야하는 기능이에요. 일단 디자인은 신경쓰지말고 만들어보세요
+
+// 지금 <div className={styles.dot}></div>코드가 중복됩니다. 간단한 컴포넌트는 파일안에 다른 컴포넌트를 만들어서 처리해도됩니다.
+// 제가 ThreeDot이라는 컴포넌트를 만들어서 사용하는 코드를 잘 살펴보시고 71줄 주석 풀어보세요
