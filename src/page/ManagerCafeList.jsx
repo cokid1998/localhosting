bfdd2234e8b770b7ccc0a226cb0b5b7ffb954a08
bakamon <excqod@gmail.com>
2024-11-05 10:24:06 +0900

    피드백 수정

--- src/page/ManagerCafeList.jsx
+++ src/page/ManagerCafeList.jsx
@@ -21,38 +21,39 @@ function ManagerCafeList() {
     setIsSearchActive(!isSearchActive);
   };
 
+  const closeSearch = () => {
+    setIsSearchActive(false);
+  };
+
   return (
     <div className={styles.managerPage}>
       <div className={styles.navbar}>
         <ChevronLeft className={styles.backIcon} onClick={handleBack} />
         <div className={styles.title}>관리자 페이지</div>
         <Search
-          className={`${styles.searchIcon} ${
-            isSearchActive ? styles.activeSearchIcon : ""
-          }`} // 여기 부분도 55줄 처럼 &&로 처리할 수 있어요
+          className={`${styles.searchIcon} ${isSearchActive ? styles.activeSearchIcon : ""}`}
           onClick={toggleSearch}
         />
       </div>
 
-      {isSearchActive && (
-        <div className={styles.searchContainer}>
-          <input
-            type="text"
-            placeholder="검색어를 입력하세요"
-            className={styles.searchInput}
-          />
-          <X className={styles.closeIcon} onClick={toggleSearch} />
-        </div>
-      )}
-
-      {!isSearchActive && (
-        <div className={styles.dataButtonContainer}>
-          <button className={styles.dataButton}>데이터 추출</button>
-        </div>
-      )}
+      <div className={styles.dataButtonContainer}>
+        <button className={styles.dataButton}>데이터 추출</button>
+        {isSearchActive && (
+          <div className={styles.searchContainer}>
+            <input
+              type="text"
+              placeholder="검색어를 입력하세요"
+              className={styles.searchInput}
+            />
+            <X className={styles.closeIcon} onClick={toggleSearch} />
+          </div>
+        )}
+      </div>
 
       <div className={styles.cafeList}>
-        {isSearchActive && <div className={styles.overlay}></div>}
+        {isSearchActive && (
+          <div className={styles.overlay} onClick={closeSearch}></div>
+        )}
         {cafes.map((cafe) => (
           <Link
             to={`/cafe/${cafe.id}`}
@@ -76,6 +77,8 @@ function ManagerCafeList() {
 }
 
 export default ManagerCafeList;
+
+
 
 /* 2024.11.04
 1. overlay와 검색컴포넌트가 따로 위치해있는데 같은 컴포넌트로 묶는게 좋아요.
