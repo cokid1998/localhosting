6dca3b52f629f94aee1b1c48258fff6e47bfe425
이태관 <itaegwan@itaegwans-MacBook-Pro.local>
2024-12-11 10:59:10 +0900

    set role

--- src/api/authAPI.js
+++ src/api/authAPI.js
@@ -1,11 +1,12 @@
 import API from "@/api/api";
+import { setCookie } from "@/util/cookies";
 
 export const loginAPI = async (id, password) => {
   const res = await API.post("/api/user/login", {
     username: id,
     password,
   });
-
+  setCookie("role", res.data.role);
   return res;
 };
 
